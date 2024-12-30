fb9a3a3441ec4b227607b539b0c2d34a912746c9
Tushar Sinha <tusharsinha@192.168.1.4>
2024-12-30 22:41:06 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Bloggary
+An engaging web development project by creating a dynamic blog website using HTML, CSS, and JavaScript. I'd like to emphasize the significance of responsive design. In the project thaings that are used like CSS flexbox and grid layouts, media queries, fluid design that ensures the website's content and layout adjust according to the screen size.
---
+++ main.js
@@ -0,0 +1,41 @@
+const responsive={
+    0:{
+        items:1
+    },
+    320:{
+        items:1
+    },
+    560:{
+        items:2
+    },
+    960:{
+        items:3
+    }
+}
+
+$(document).ready(function(){
+    
+    $nav=$('.nav');
+    $togglecollapse= $('.toggle-collapse');
+
+    /* After clicking on toggle menu */
+    $togglecollapse.click(function(){
+        $nav.toggleClass('collapse');
+    })
+
+    // owl-carousel for blog
+    $('.owl-carousel').owlCarousel({
+     loop: true,
+     autoplay: true, 
+     autoplayTimeout:3000,  
+     dots: false,
+     nav: true,
+     navText: [$('.owl-navigation .owl-nav-prev'), $('.owl-navigation .owl-nav-next')],
+     responsive: responsive
+    });
+    
+    // AOS instance
+    AOS.init()
+
+});
+
